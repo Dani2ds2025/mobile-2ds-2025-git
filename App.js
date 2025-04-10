fcd516533b24f56b64a5165c06cbdd13e5a4a4c1
Dani2ds2025 <daniele.dudat@escola.pr.gov.br>
2025-04-10 10:35:32 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import Aula01 from "./src/aula01";
+//import Aula01 from "./src/aula01";
+
+import Aula02 from "./src/aula02";
 
 export default function App() {
   return (
-    <Aula01 />
+    //<Aula01 />
+    <Aula02/>
   );
 }
+
 
 
