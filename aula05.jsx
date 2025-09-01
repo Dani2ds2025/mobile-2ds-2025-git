cfa4482b9552c7fd8ef7ba0e50ba81ddb1cd8447
Dani2ds2025 <daniele.dudat@escola.pr.gov.br>
2025-09-01 07:47:47 -0300

    Add files via upload

--- aula05.jsx
+++ aula05.jsx
@@ -1,29 +1,53 @@
-import React, { Component } from "react";
-import { View, Text, Stylesheet } from "react-native";
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-class Aula05 extends Component {
-    render() {
-        return(
-            <View style={Stylesheet.container}>
-                <View style={Stylesheet.caixa1}>1</View>
-                <View style={Stylesheet.caixa2}>2</View>
-                <View style={Stylesheet.caixa3}>3</View>
-            </View>
-        );
-    }
+class Aula05  extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={ styles.caixa1 }>1</View>
+        <View style={ styles.caixa2 }>
+            <Image />
+        </View>
+        <View style={ styles.caixa3 }>3</View>
+      </View>
+    );
+  }
 }
 
 export default Aula05;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignitems: "center",
-        justifycontent: "center"
+  container: {
+     flex: 1,
+     backgroundColor: 'white',
+  },
 
-        
-    }
+  caixa1: {
+    height: 100,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    fontSize: 20
+  },
+
+  caixa2: {
+   flex: 1,
+   backgroundColor: 'green',
+   color: 'white',
+   fontSize: 20,
+   justifyContent: 'center',
+   alignItems: 'center'
+},
+  caixa3: {
+
+   backgroundColor: 'blue',
+   height: 100,
+   alignItems: 'center',
+   justifyContent: 'center'
+   color: 'white',
+   fontSize: 20
 }
+})
 
-)
